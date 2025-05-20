3abffe552975e455dfdb437568ef04e88a2e1202
nasrat <nasratnejat03@gmail.com>
2025-05-20 18:30:46 +0530

    final

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,19 @@
+import { Collection } from '@/components/shared/Collection'
 import { navLinks } from '@/constants'
+import { getAllImages } from '@/lib/actions/image.actions'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Home = () => {
+const Home = async ({ searchParams }: SearchParamProps) => {
+	const page = Number(searchParams?.page) || 1
+	const searchQuery = (searchParams?.query as string) || ''
+	const images = await getAllImages({ page, searchQuery })
 	return (
 		<div>
 			<section className='home'>
 				<h1 className='home-heading'>
-					Unleash Your Creative Vision with Imaginify
+					Unleash Your Creative Vision with Ishrun
 				</h1>
 				<ul className='flex-center w-full gap-20'>
 					{navLinks.slice(1, 5).map(link => (
@@ -25,12 +30,12 @@ const Home = () => {
 				</ul>
 			</section>
 			<section className='sm:mt-12'>
-				{/* <Collection 
-          hasSearch={true}
-          images={images?.data}
-          totalPages={images?.totalPage}
-          page={page}
-        /> */}
+				<Collection
+					hasSearch={true}
+					images={images?.data}
+					totalPages={images?.totalPage}
+					page={page}
+				/>
 			</section>
 		</div>
 	)
--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ const IBMPlex = IBM_Plex_Sans({
 })
 
 export const metadata: Metadata = {
-	title: 'Imaginify',
-	description: 'AI-powered image generator',
+	title: 'Ishrun',
+	description: 'Your Ai Companion',
 }
 
 export default function RootLayout({
